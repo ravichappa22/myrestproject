98d421e645ff16990aded0aa5c5a5f8591071852
ravchappa22 <chapparavi22@gmail.com>
2017-11-12 17:58:23 -0500

    added SOP

--- src/main/java/com/example/rest/MyRestController.java
+++ src/main/java/com/example/rest/MyRestController.java
@@ -4,6 +4,8 @@ import java.security.Principal;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configurers.SecurityContextConfigurer;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +20,9 @@ public class MyRestController {
 	@RequestMapping("/name")
 	@PreAuthorize("hasRole('ROLE_write')")
 	public Principal getMyName(Principal user){
-		System.out.println("inside getMyName>>>>");
+		
+		System.out.println("inside getMyName>>>>" + SecurityContextHolder.getContext().getAuthentication().getName());
+		System.out.println("Authorities="+ SecurityContextHolder.getContext().getAuthentication().getAuthorities());
 		return user;
 	}
 
